7042c68314ad704afa570b94588d3da629598c44
Richard J Telford <richard.telford@bio.uib.no>
2017-03-24 14:09:06 +0100

    add c: to path

--- R/find.ij.R
+++ R/find.ij.R
@@ -2,7 +2,7 @@
 find.ij <- function(ostype = .Platform$OS.type){
 
   if (ostype == "windows"){
-    if(file.exists("\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
+    if(file.exists("c:\\Program Files\\ImageJ\\ij.jar")!=T || file.exists("c:\\Program Files\\ImageJ\\jre\\bin\\java.exe")!=T) {
       warning("ij.jar or java were not found in the common install location on your system; When you run run.ij, specify the path to ImageJ directory or try installing ImageJ bundled with Java to C:\\Program Files\\ ")
       return("ImageJ not found")} else return("C:\\Program Files\\ImageJ\\")
     } else {
